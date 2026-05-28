92ba47b3cb0a9eb720d7e4b2f1d9768b2d213f1d
alymughal512-max <alymughal512@gmail.com>
2026-05-28 00:34:01 -0700

    Update bot activity name to 'listening to parrot mc'

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "listining to parrot mc",
         // Activity type number (0 = Playing).
         type: 0, 
       },
